a08882e13859659d8ca5dbf2006e758995711ee4
Gayatri Tavva <gayatri.tavva@gmail.com>
2019-02-22 15:29:20 -0800

    changes done by gayatri(peer)

--- lab-03-assignment/js/app.js
+++ lab-03-assignment/js/app.js
@@ -1,58 +1,72 @@
-var user = getUsername();
-	console.log(user);
-	alert('Welcome ' + user + ' to my personal blog');
-var playGame = okToPlay();
-	play(playGame);
-	var car = findMyCar();
-	favouriteCar(car);
-	var bornCity = getBirthRegion();
-	birthPlace(bornCity);
-	var sport = getFavouriteSport();
-	favouriteSport(sport);
-    var number;
-    var counter = 1;
-   while (number !== 08) {
-    number = parseInt(prompt('guess my favorite number'));
-    if (number < 08) {
-        alert('sorry you guessed too low');
-        counter++;
-    } else if (number > 08) {
-        alert('you guessed too high');
-        counter++;
-    } else if (isNaN(number) || number === null) {
-        alert('please enter an actual number');
-        counter++;
-    }
+ //Declaring Questions
+ var carQuestion = 'Is Porsche one of my favourite car (Y/N)?';
+ var sportsQuestion = 'Is American football my favourite sport (Y/N)?';
+ var cityQuestion = 'Is Switzerland my favourite vacation spot (Y/N)?';
+ var shopQuestion = 'Is Amazon my favourite first shopping spot (Y/N)?';
+ var socialQuestion = 'Do I like to spend my time on Instagram (Y/N)?';
+ //Declaring Array
+var questions = [carQuestion,sportsQuestion,cityQuestion,shopQuestion,socialQuestion];
+
+ //Declaring Array for Answers
+ var answers = ['Y','N','N','Y','Y'];
+
+//Ask user Name
+ var userNameQuestion = 'Please enter your name';
+ console.log('hi');
+ var userName = getUserInput(userNameQuestion);
+ console.log('after');
+ alert('Welcome ' + userName + ' to my personal blog');
+
+//Check if user is Intrested to play
+ var validateIfOkToPlay = 'Interested in Guessing things about Myself(Y/N)?';
+ var playGame = getUserInput(validateIfOkToPlay);
+ if (play(playGame)){
+	askQuestions(questions);
+	alert('Thank you for playing Guessing Game about Myself!!!');
+ }else{
+	 alert('See you soon!');
+ }
+ //Ask questions
+ function askQuestions(questions){
+	
+	 for(var i= 0;i<questions.length;i++){
+	var userGuess= getUserInput(questions[i]);
+	console.log('Answer to Question ' + questions[i] + 'is ' + userGuess);
+
+	if (validateCorrectAnswer(userGuess,answers[i])){
+		alert('Yayyyy! You know me perfectly');
+	}else {
+		console.log('in else');
+		alert('come on yaar! you are almost there lets try again ');
+		var userGuess= getUserInput(questions[i]);
+		if (validateCorrectAnswer(userGuess,answers[i])){
+			alert('Yayyyy! You know me perfectly');
+		}else {
+			alert('Ok No worries! lets move to another question ');
+		}
+
+	 }
+ }
 }
 
-console.log('total guesses until i got it:', counter);
+ //Validate User answer
+ function validateCorrectAnswer(userAnswer,correctAnswer){
 
-	alert('Thank you for playing Guessing Game about Myself!!!');
+	 if(userAnswer.toUpperCase() === correctAnswer.toUpperCase()){
+		 console.log('validate true');
+		return true;
+	 }else{
+		console.log('validate false');
+       return false;
+	 }
 
-function getUsername() {
-	user = window.prompt('Please enter your name');
-	return user;
-};
-
-function okToPlay() {
-		playGame = window.prompt('Interested in Guessing things about Myself(Y/N)?');
-		return playGame;
-};
-
-function findMyCar() {
-	car = window.prompt('Guess my favourite car');
-	return car;
-};
-
-function getBirthRegion() {
-	birthPlace = window.prompt('Guess my Birth Village');
-	return birthPlace;
-};
-
-function getFavouriteSport() {
-	sport = window.prompt('Guess my Favourite Sport');
-	return sport;
-};
+ }
+//prompt the answer to question
+	function getUserInput(question) {
+		console.log(question);
+	   var answer = prompt(question);
+	   return answer;
+	}
 
 function play(playGame){
 	playGame = this.playGame;
@@ -60,55 +74,17 @@ function play(playGame){
 	var option1 = 'y';
     var option2 = 'yes';
 	if (playGame === null) {
-		alert('No User Input. Please enter')
+		alert('No User Input. Please enter');
+		return false;
 	}
 	else if (playGame === option1 || playGame === option2)
   {
 		alert('All the very Best!! Lets play the game');
+		return true;
 	}
 	else {
-		alert('Please enter Yes or Y to play game');
+		alert('Invalid Input');
+		return false;
 	}
 };
 
-function favouriteCar(car){
-	car = this.car;
-	var favouriteCar = ['Porsche', 'Lamborghini', 'BMW', 'Tesla', 'Mercedes'];
-    var flag;
-    for (var i = 0; i < favouriteCar.length; i++) {
-    if (car === favouriteCar[i]) {
-        alert('you got it right!');
-        flag = true;
-        break;
-}
-};
-}
-function birthPlace(bornCity){
-	bornCity = this.bornCity;
-  	bornCity = bornCity.toLowerCase();
-	var expectedAnswer = 'Ongole';
-
-	if (bornCity === null) {
-		alert('No User Input. Please enter')
-	}
-	else if (bornCity === expectedAnswer) {
-		alert('Great and Perfect Guess!!');
-	}
-	else {
-		alert('Oh no!!Please Play again to guess correctly');
-	}
-};
-
-function favouriteSport(sport){
-	sport = this.sport;
-	var expectedAnswer = 'cricket';
-	if (sport === null) {
-		alert('No User Input. Please enter')
-	}
-	else if (sport === expectedAnswer) {
-		alert('Great and Perfect Guess!!');
-	}
-	else {
-		alert('Oh no!!Please Play again to guess correctly');
-	}
-};
